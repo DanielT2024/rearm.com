48e7be397b2badb00409188b20ba5ee601231f0d
Daniel <edundaniel101@gmail.com>
2025-05-27 14:25:57 +0100

    done final

--- core/settings.py
+++ core/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Project-level static
-STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')  # production level static collection
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')  # production level static collection
 
 
 # media/settings.py
